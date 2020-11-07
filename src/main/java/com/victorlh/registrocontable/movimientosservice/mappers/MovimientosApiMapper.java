58c3db218033d9bea5bf23e033f354e92a139e27
Victor Latorre <victor.latorre.herreros@gmail.com>
2020-11-07 14:38:05 +0100

    Creado mappers de movimientos dto api

---
+++ src/main/java/com/victorlh/registrocontable/movimientosservice/mappers/MovimientosApiMapper.java
@@ -0,0 +1,28 @@
+package com.victorlh.registrocontable.movimientosservice.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import com.victorlh.registrocontable.movimientosservice.api.dto.request.MovimientoRequestDTO;
+import com.victorlh.registrocontable.movimientosservice.api.dto.response.MovimientoResponseDTO;
+import com.victorlh.registrocontable.movimientosservice.domain.conf.ETipoMovimiento;
+import com.victorlh.registrocontable.movimientosservice.domain.model.Movimiento;
+import com.victorlh.registrocontable.movimientosservice.domain.model.MovimientoBuilder;
+
+@Mapper(componentModel = "spring")
+public interface MovimientosApiMapper {
+
+	@Mapping(target = "tipoMovimientoId", source = "movimiento.tipoMovimiento")
+	MovimientoResponseDTO movimientoToMovimientoResponse(Movimiento movimiento);
+	
+	@Mapping(target = "movimientoContable", ignore = true)
+	@Mapping(target = "movimietosAsociados", ignore = true)
+	@Mapping(target = "tipoMovimiento", source = "request.tipoMovimientoId", qualifiedByName = "idToTipoMovimiento")
+	MovimientoBuilder movimientoRequestToMovimientoBuilder(MovimientoRequestDTO request);
+	
+	@Named("idToTipoMovimiento")
+	default ETipoMovimiento idToTipoMovimiento(String tipoMovimientoId) {
+		return ETipoMovimiento.findById(tipoMovimientoId).orElse(null);
+	}
+}
